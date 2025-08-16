41e93294ad9f664f5db130ad4893155772e65b34
Aflouat <aflouat@gmail.com>
2025-08-16 22:22:17 +0200

    squelette MYQ 12

--- front/src/app/features/game/game-routing.module.ts
+++ front/src/app/features/game/game-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {QuizComponent} from './components/quiz/quiz.component';
+import { QuizComponent } from './components/quiz/quiz.component';
+import { QuizListComponent } from './quiz-list/quiz-list.component';
 
 
 
 const routes: Routes = [
   { title: 'game', path: 'quiz/1', component: QuizComponent },
+  { title: 'quiz-list', path: 'quiz', component: QuizListComponent },
 ];
 
 @NgModule({
