262ab95bb181545b26abc8ab9129c755f18db924
Aflouat <aflouat@gmail.com>
2025-08-16 13:15:35 +0200

    lien page quiz

--- front/src/app/features/game/components/quiz/quiz.component.ts
+++ front/src/app/features/game/components/quiz/quiz.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { QuizService } from '../../services/quiz.service';
 
 @Component({
   selector: 'app-quiz',
+    standalone: false,
   templateUrl: './quiz.component.html',
-  styleUrl: './quiz.component.scss',
-  standalone: false
 })
-export class QuizComponent {
+export class QuizComponent implements OnInit {
+  quiz: any;
+  id = 1
+  player = 'Omar';
 
+  constructor(private quizService: QuizService) {}
+
+  ngOnInit() {
+    this.quizService.getQuizById(this.id).subscribe(data => {
+      this.quiz = data;
+    });
+  }
+
+  checkAnswer(question: any, answerId: number) {
+    if (answerId === question.correctAnswerId) {
+      alert('Bonne réponse 🎉');
+    } else {
+      alert('Mauvaise réponse ❌');
+    }
+  }
 }
